35881cf763e095251dc9bd21577210b72fa1d774
Julio Saraiva <contato@juliosaraiva.com.br>
2020-08-27 05:49:37 -0300

    chore: Project Setup

---
+++ commands/gen_secret.py
@@ -0,0 +1,8 @@
+"""
+Django SECRET_KEY generator.
+"""
+from django.utils.crypto import get_random_string
+
+
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+print(get_random_string(50, chars))
