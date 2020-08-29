5aa404771087f83c664eb1fe85dcaf4bf240ecfa
Julio Saraiva <contato@juliosaraiva.com.br>
2020-08-29 04:32:18 -0300

    fix: Changed some relationships

--- src/gestao_rh/apps/documents/models.py
+++ src/gestao_rh/apps/documents/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Document(models.Model):
     description = models.CharField(max_length=255)
+    owner = models.ForeignKey('employees.Employee',
+                              on_delete=models.PROTECT)
 
     def __str__(self):
         return self.description
--- src/gestao_rh/apps/overtime/models.py
+++ src/gestao_rh/apps/overtime/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Overtime(models.Model):
     reason = models.CharField(max_length=80)
+    employee = models.ForeignKey('employees.Employee', on_delete=models.CASCADE)
     starts = models.DateTimeField()
     ends = models.DateTimeField()
 
