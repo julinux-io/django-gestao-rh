10ce65043f0fc1ac1f2ac20a722ab48c5a1796da
Julio Saraiva <contato@juliosaraiva.com.br>
2020-09-20 18:40:43 -0300

    fix: :bug: Fixed url to overtime list

--- src/gestao_rh/apps/overtime/urls.py
+++ src/gestao_rh/apps/overtime/urls.py
@@ -6,7 +6,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', OvertimeList.as_view(), name='overtime-list'),
+    path('', OvertimeList.as_view(), name='overtimes-list'),
     path('new/', OvertimeCreate.as_view(), name='overtime-create'),
     path('edit/<int:pk>/', OvertimeUpdate.as_view(), name='overtime-update'),
     path('delete/<int:pk>/', OvertimeDelete.as_view(), name='overtime-delete'),
